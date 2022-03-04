72a059b3fc26b2fc9bf47fe8ec334c05429c7191
Jaroslaw Drabek <jarekdrabek@gmail.com>
2022-03-04 15:33:44 +0100

    more lottery tests

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,15 +1,15 @@
+import pytest
 from brownie import Lottery, accounts, VRFCoordinatorV2Mock
+from brownie.exceptions import VirtualMachineError
 from brownie.network.web3 import Web3
-
 
 
 def test_lottery_workflow():
     # Arrange
-    lottery_owner_account = accounts[0]
-    first_player_account = accounts[1]
-    second_player_account = accounts[2]
-    third_player_account = accounts[3]
-    lottery_contract, vrf_coordinator = __deploy_and_get_lottery_contract_and_dependencies(lottery_owner_account)
+    first_player_account = accounts[0]
+    second_player_account = accounts[1]
+    third_player_account = accounts[2]
+    lottery_contract, vrf_coordinator = __deploy_and_get_lottery_contract_and_dependencies(first_player_account)
 
     #Act
     tx = lottery_contract.buyCouponAnTryToWin({"from": first_player_account, "value": Web3.toWei(0.001, 'ether')})
@@ -26,6 +26,58 @@ def test_lottery_workflow():
     assert first_player_account.balance() == Web3.toWei(99.999, 'ether')
     assert second_player_account.balance() == Web3.toWei(99.999, 'ether')
     assert third_player_account.balance() == Web3.toWei(100.002, 'ether')
+
+
+def test_cannot_buy_another_coupon_whe_the_previous_one_was_not_resolved():
+    # Arrange
+    first_player_account = accounts[0]
+    second_player_account = accounts[1]
+    lottery_contract, vrf_coordinator = __deploy_and_get_lottery_contract_and_dependencies(first_player_account)
+
+    # Act and Assert
+    lottery_contract.buyCouponAnTryToWin({"from": first_player_account, "value": Web3.toWei(0.001, 'ether')})
+    with pytest.raises(VirtualMachineError):
+        lottery_contract.buyCouponAnTryToWin({"from": second_player_account, "value": Web3.toWei(0.001, 'ether')})
+
+
+def test_you_can_start_new_lottery_when_the_previous_one_was_finished():
+    # Arrange
+    first_player_account = accounts[0]
+    second_player_account = accounts[1]
+    lottery_contract, vrf_coordinator = __deploy_and_get_lottery_contract_and_dependencies(first_player_account)
+
+    # Act
+    tx = lottery_contract.buyCouponAnTryToWin({"from": first_player_account, "value": Web3.toWei(0.001, 'ether')})
+    __fullfill_lottery_vrf_coordinator_with_given_random_result2(tx.return_value, lottery_contract, vrf_coordinator, 400)
+
+    tx = lottery_contract.buyCouponAnTryToWin({"from": second_player_account, "value": Web3.toWei(0.001, 'ether')})
+    __fullfill_lottery_vrf_coordinator_with_given_random_result2(tx.return_value, lottery_contract, vrf_coordinator, 900)
+
+    #Assert
+    assert lottery_contract.balance() == Web3.toWei(0.001, 'ether')
+
+
+def test_coupon_for_more_then_one_million_gwei():
+    # Arrange
+    first_player_account = accounts[0]
+    testing_contract,_ = __deploy_and_get_lottery_contract_and_dependencies(first_player_account)
+
+
+    # Act and Assert
+    with pytest.raises(VirtualMachineError):
+        testing_contract.buyCouponAnTryToWin(
+            {"from": first_player_account, "value": Web3.toWei(0.001, 'ether') + 1}
+        )
+
+
+def test_coupon_for_less_then_one_million_gwei():
+    # Arrange
+    first_player_account = accounts[0]
+    testing_contract,_ = __deploy_and_get_lottery_contract_and_dependencies(first_player_account)
+
+    # Act and Assert
+    with pytest.raises(VirtualMachineError):
+        testing_contract.buyCouponAnTryToWin({"from": first_player_account, "value": Web3.toWei(0.001, 'ether') - 1})
 
 
 def __deploy_and_get_lottery_contract_and_dependencies(deploying_account):
